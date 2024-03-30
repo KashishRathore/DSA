8f2707f1034d5cbeba7098a2e0aa001df207ab10
KashishRathore <kashishrathore0111@gmail.com>
2024-03-30 19:43:51 +0530

    New Commit

---
+++ Java/Recursion/Recursion_Advanced/Invite_Guests.java
@@ -0,0 +1,25 @@
+package Recursion.Recursion_Advanced;
+
+// Q -> Find the number of ways in which you can invite n people to your part, single or in pairs
+// n = 4
+
+public class Invite_Guests {
+    public static int callGuests(int n){
+        if(n <= 1){
+            return 1;
+        }
+
+        //single
+        int way1 = callGuests(n-1);
+
+        //pairs
+        int way2 = (n-1) * callGuests(n-2);
+
+        return way1 + way2;
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(callGuests(n));
+    }
+}
+
