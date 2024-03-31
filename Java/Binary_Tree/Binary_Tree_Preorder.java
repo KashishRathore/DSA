e234e34b864dd3af186d2afbfbbdb54fee046647
KashishRathore <kashishrathore0111@gmail.com>
2024-03-31 21:22:49 +0530

    Tree

--- Java/Binary_Tree/Binary_Tree_Preorder.java
+++ Java/Binary_Tree/Binary_Tree_Preorder.java
@@ -1,5 +1,7 @@
 package Binary_Tree;
 
+//pre order -> root --> left order --> right order
+
 public class Binary_Tree_Preorder {
     static class Node{
         int data;
@@ -27,10 +29,22 @@ public class Binary_Tree_Preorder {
            return newNode;
         }
     }
+
+    public static void preOrder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
+        preOrder(root);
+
+
     }
 }
--- Java/Linked_list/LinkedList_Question/LinkedList_Question1.java
+++ Java/Linked_list/LinkedList_Question/LinkedList_Question1.java
@@ -3,48 +3,45 @@ package Linked_list.LinkedList_Question;
 // Q -> Find and Delete the nth Node from the end of Linked List   
 
 public class LinkedList_Question1 {
-   
-        class ListNode {
-            String data;
-            ListNode next;
 
-            ListNode(String data) {
-                this.data = data;
-                this.next = null;
-            }
+    class ListNode {
+        String data;
+        ListNode next;
+
+        ListNode(String data) {
+            this.data = data;
+            this.next = null;
         }
-
-        public ListNode removeNthFromEnd(ListNode head, int n) {
-            if (head.next == null) {
-                return null;
-            }
-
-            // Finding the size of list
-            int size = 0;
-            ListNode curr = head;
-            while (curr != null) {
-                curr = curr.next;
-                size++;
-            }
-
-            if (n == size) {
-                return head.next;
-            }
-
-            // Finding the previous node
-            int idxToSearch = size - n;
-            ListNode prev = head;
-            int i = 0;
-            while (i < idxToSearch) {
-                prev = prev.next;
-                i++;
-            }
-
-            prev.next = prev.next.next;
-
-            return head;
-        }
-
-        
     }
 
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head.next == null) {
+            return null;
+        }
+
+        // Finding the size of list
+        int size = 0;
+        ListNode curr = head;
+        while (curr != null) {
+            curr = curr.next;
+            size++;
+        }
+
+        if (n == size) {
+            return head.next;
+        }
+
+        // Finding the previous node
+        int idxToSearch = size - n;
+        ListNode prev = head;
+        int i = 0;
+        while (i < idxToSearch) {
+            prev = prev.next;
+            i++;
+        }
+
+        prev.next = prev.next.next;
+
+        return head;
+    }
+}
