a8fbd4fe23a05505dcc41cddadcdcf23946d9722
KashishRathore <kashishrathore0111@gmail.com>
2024-04-05 00:25:31 +0530

    SubTree

--- Java/Binary_Tree/Subtree_Of_Another_Tree.java
+++ Java/Binary_Tree/Subtree_Of_Another_Tree.java
@@ -1,7 +1,56 @@
 package Binary_Tree;
 
 public class Subtree_Of_Another_Tree {
-    public boolean isTree(){
+    static class TreeNode{
+        int data;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){}
+        TreeNode(int data){
+            this.data = data;
+        }
+        TreeNode(int data, TreeNode left, TreeNode right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isIdentical(TreeNode root, TreeNode subRoot){
+        if(root == null && subRoot == null){
+           return true;
+        }
+
+        if(root == null || subRoot == null){
+            return false;
+        }
+
+        if(root.data == subRoot.data){
+            return isIdentical(root.left, subRoot) && isIdentical(root.right, subRoot);
+        }
+
+        return false;
+    }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot){
+        if(subRoot == null){
+           return true;
+        }
+
+        if(root == null){
+            return false;
+        }
+
+        if(root.data == subRoot.data){
+           if(isIdentical(root, subRoot)){
+            return true;
+           }
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public static void main(String[] args) {
         
     }
 }
