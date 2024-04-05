41006cfedb328779c15c7c0296ec5a861c363707
KashishRathore <kashishrathore0111@gmail.com>
2024-04-05 23:58:16 +0530

    Binary tree inserting value

---
+++ Java/Tree/Binary_Search_Tree/Building_BST.java
@@ -0,0 +1,51 @@
+package Tree.Binary_Search_Tree;
+
+public class Building_BST {
+    static class Node {
+
+        int data;
+        Node left;
+        Node right;
+
+        Node(int key) {
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        if (root.data > key) {
+            // left subtree
+            root.left = insert(root.left, key);
+        } 
+        else {
+            root.right = insert(root.right, key);
+        }
+
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.println(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        int keys[] = { 5, 1, 3, 4, 2, 7 };
+        Node root = null;
+        for (int i = 0; i < keys.length; i++) {
+            root = insert(root, keys[i]);
+        }
+
+        inorder(root);
+    }
+}
