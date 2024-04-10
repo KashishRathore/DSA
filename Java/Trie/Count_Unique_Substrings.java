ef1196f64d86334dd3d5c8f3f8d81be66a0534de
KashishRathore <kashishrathore0111@gmail.com>
2024-04-10 22:55:42 +0530

    New Commit

---
+++ Java/Trie/Count_Unique_Substrings.java
@@ -0,0 +1,16 @@
+package Trie;
+
+// Q -> Given a string of length n of lowercase alphabet characters, we need to count total no. of distinct substrings of this string.
+
+public class Count_Unique_Substrings {
+    // prefix-suffix concept
+    // all prefix of all suffix 
+    // all suffix of all prefix
+
+    // step 1 -> Find all suffix of string
+    // step 2 -> Create a trie from suffix
+    // step 3 -> Total node of trie = count of unique prefix
+    public static void main(String[] args) {
+
+    }
+}
