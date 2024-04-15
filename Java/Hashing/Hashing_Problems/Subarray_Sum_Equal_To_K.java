213c9d0e6f4e01109106e5a64bc15a7a35c499de
KashishRathore <kashishrathore0111@gmail.com>
2024-04-15 23:58:56 +0530

    New Commit

---
+++ Java/Hashing/Hashing_Problems/Subarray_Sum_Equal_To_K.java
@@ -0,0 +1,10 @@
+package Hashing.Hashing_Problems;
+
+import java.util.HashMap;
+
+public class Subarray_Sum_Equal_To_K {
+     public static void main(String[] args) {
+        int arr[] = {10, 2, -2, -20, 10};
+        HashMap<Integer, Integer> map = new HashMap<>();
+     }
+}
